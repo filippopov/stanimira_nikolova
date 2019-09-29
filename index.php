a7195d0947fb9124d77df609992750d23bcbb056
DESKTOP-FQCLTKM\Popov <popovfilip444@gmail.com>
2019-09-29 12:51:19 +0300

    fix loading

--- index.php
+++ index.php
@@ -60,29 +60,31 @@ $dbInstanceName = 'default';
     $dbInstanceName
 );
 $controlerBigLetar = 'StanimiraNikolova\Controllers\\' . ucfirst($controllerName) . 'Controller';
-if (class_exists($controlerBigLetar)) {
-    $mvcContext = new \StanimiraNikolova\Core\MVC\MVCContext($controllerName, $actionName, $self, $args, $getParams);
 
-    $app = new \StanimiraNikolova\Core\Application($mvcContext);
+$controllerName = ($controllerName == '') ? 'home' : $controllerName;
+$actionName = ($actionName == '') ? 'index' : $actionName;
 
-    $app->addClass(\StanimiraNikolova\Core\MVC\MVCContext::class, $mvcContext);
-    $app->addClass(\StanimiraNikolova\Adapter\DatabaseInterface::class, \StanimiraNikolova\Adapter\Database::getInstance($dbInstanceName));
-    $app->addClass(\StanimiraNikolova\Core\MVC\SessionInterface::class, new \StanimiraNikolova\Core\MVC\Session($_SESSION));
+$mvcContext = new \StanimiraNikolova\Core\MVC\MVCContext($controllerName, $actionName, $self, $args, $getParams);
 
-    $app->registerDependency(\StanimiraNikolova\Core\ViewInterface::class, \StanimiraNikolova\Core\View::class);
-    $app->registerDependency(\StanimiraNikolova\Services\User\UserServiceInterface::class, \StanimiraNikolova\Services\User\UserService::class);
-    $app->registerDependency(\StanimiraNikolova\Services\Application\EncryptionServiceInterface::class, \StanimiraNikolova\Services\Application\BCryptEncryptionService::class);
-    $app->registerDependency(\StanimiraNikolova\Services\Application\AuthenticationServiceInterface::class, \StanimiraNikolova\Services\Application\AuthenticationService::class);
-    $app->registerDependency(\StanimiraNikolova\Services\Application\ResponseServiceInterface::class, \StanimiraNikolova\Services\Application\ResponseService::class);
-    $app->registerDependency(\StanimiraNikolova\Repositories\User\UserRepositoryInterface::class, \StanimiraNikolova\Repositories\User\UserRepository::class);
-    $app->registerDependency(\StanimiraNikolova\Services\Menu\MenuServiceInterface::class, \StanimiraNikolova\Services\Menu\MenuService::class);
-    $app->registerDependency(\StanimiraNikolova\Repositories\Menu\MenuRepositoryInterface::class, \StanimiraNikolova\Repositories\Menu\MenuRepository::class);
-    $app->registerDependency(\StanimiraNikolova\Services\PageData\PageDataServiceInterface::class, \StanimiraNikolova\Services\PageData\PageDataService::class);
-    $app->registerDependency(\StanimiraNikolova\Repositories\PageData\PageDataRepositoryInterface::class, \StanimiraNikolova\Repositories\PageData\PageDataRepository::class);
+$app = new \StanimiraNikolova\Core\Application($mvcContext);
 
-    try {
-        $app->start();
-    } catch (\Throwable $t) {
-        echo $t->getMessage();
-    }
+$app->addClass(\StanimiraNikolova\Core\MVC\MVCContext::class, $mvcContext);
+$app->addClass(\StanimiraNikolova\Adapter\DatabaseInterface::class, \StanimiraNikolova\Adapter\Database::getInstance($dbInstanceName));
+$app->addClass(\StanimiraNikolova\Core\MVC\SessionInterface::class, new \StanimiraNikolova\Core\MVC\Session($_SESSION));
+
+$app->registerDependency(\StanimiraNikolova\Core\ViewInterface::class, \StanimiraNikolova\Core\View::class);
+$app->registerDependency(\StanimiraNikolova\Services\User\UserServiceInterface::class, \StanimiraNikolova\Services\User\UserService::class);
+$app->registerDependency(\StanimiraNikolova\Services\Application\EncryptionServiceInterface::class, \StanimiraNikolova\Services\Application\BCryptEncryptionService::class);
+$app->registerDependency(\StanimiraNikolova\Services\Application\AuthenticationServiceInterface::class, \StanimiraNikolova\Services\Application\AuthenticationService::class);
+$app->registerDependency(\StanimiraNikolova\Services\Application\ResponseServiceInterface::class, \StanimiraNikolova\Services\Application\ResponseService::class);
+$app->registerDependency(\StanimiraNikolova\Repositories\User\UserRepositoryInterface::class, \StanimiraNikolova\Repositories\User\UserRepository::class);
+$app->registerDependency(\StanimiraNikolova\Services\Menu\MenuServiceInterface::class, \StanimiraNikolova\Services\Menu\MenuService::class);
+$app->registerDependency(\StanimiraNikolova\Repositories\Menu\MenuRepositoryInterface::class, \StanimiraNikolova\Repositories\Menu\MenuRepository::class);
+$app->registerDependency(\StanimiraNikolova\Services\PageData\PageDataServiceInterface::class, \StanimiraNikolova\Services\PageData\PageDataService::class);
+$app->registerDependency(\StanimiraNikolova\Repositories\PageData\PageDataRepositoryInterface::class, \StanimiraNikolova\Repositories\PageData\PageDataRepository::class);
+
+try {
+    $app->start();
+} catch (\Throwable $t) {
+    echo $t->getMessage();
 }
